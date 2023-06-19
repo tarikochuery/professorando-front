3da49c98d84132fc36446c7cbc5a6722da08eec0
tarikochuery <ttarikechuery@gmail.com>
2023-06-19 05:40:05 -0300

    First Commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="h-screen bg-black w-full flex">
+      <div className="w-5 border h-5 bg-black text-xl">box 1</div>
+      <div className="w-5 border h-5 bg-black">box 2</div>
+      <div className="w-5 border h-5 bg-black">box 3</div>      
+    </div>
+  )
+}
+
+export default Home
